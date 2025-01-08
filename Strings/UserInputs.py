832753a4d68f1444af03cb1ccbda1c8d08aa201a
Sunil5883 <gsunil2088@gmail.com>
2025-01-08 19:26:14 +0530

    Initial Commit

---
+++ Strings/UserInputs.py
@@ -0,0 +1,5 @@
+#input()will always takes an input as a string:
+num1 = int(input('Enter num1'))
+num2 = int(input('Enter num2'))
+result = num1 + num2
+print(f'Addition of {num1} and {num2} is:', result)
---
+++ VariablesDataTypes/DataTypes.py
@@ -0,0 +1,10 @@
+# In python there is only four data types 
+# 1.string 2. int 3. float 4. bool
+a = 10
+b = "Kodnest"
+print("Type of b is", type(b))
+print("Type of a:", type(a))
+c = True
+print("Type of 'c':", type(c))
+d = 2.2
+print(d, type(d))
